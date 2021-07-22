834c50b4cfe77e25268347a59726ded57b01ad55
rahul <rahul@kliftox.com>
2021-07-22 19:41:54 -0400

    use public private route for login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import PrivateRoute from './components/privateRoute';
+import PublicRoute from './components/publicRoute';
 import Home from  './components/home';
 import Login from './components/login';
+
  
   import {
     BrowserRouter as Router,
@@ -14,8 +17,10 @@ import Login from './components/login';
       <Router>
         <div>
             <Switch>
-              <Route path='/Home'    component={Home} />
-              <Route path='/Login'   component={Login} />
+              {/* <Route path='/Home'    component={Home} />
+              <Route path='/Login'   component={Login} /> */}
+              <PublicRoute restricted={true} component={Login} path="/login" exact />
+              <PrivateRoute component={Home} path="/home" exact />
             </Switch>
          </div>
       </Router>
