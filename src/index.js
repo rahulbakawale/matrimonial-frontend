ad9749cf45b970427610f23c5b97a118ca043835
rahul <rahul@kliftox.com>
2021-08-03 13:41:15 -0400

    install ToastContainew and access CSS and import toastify

--- src/index.js
+++ src/index.js
@@ -3,13 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const Main = () => {
+
+
+
+  return(<>
+      <ToastContainer/>
+      <App/>
+  </>)
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Main />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
