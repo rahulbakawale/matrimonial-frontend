30d1dee9d3dc132f4063ddd88c294f7ec2365ceb
rahul <rahul@kliftox.com>
2021-08-03 13:29:56 -0400

    create axiosInstance and pass access-token Udi and client id

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://wedded-raghu.herokuapp.com/api/v1/mobiles/verify',
-    timeout: 1000,
+    baseURL: process.env.REACT_APP_API_URL,
+    timeout: 20000,
     headers: {
         'access-token': localStorage.user && JSON.parse(localStorage.user)['access-token'],
         'client': localStorage.user && JSON.parse(localStorage.user)['client'],
         'uid': localStorage.user && JSON.parse(localStorage.user)['uid'],
     }
-  });
+});
 
 export default axiosInstance
