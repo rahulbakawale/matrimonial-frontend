5340e0c9c61ee5f5ec0f8edb763368ed49ccb015
rahul <rahul@kliftox.com>
2021-08-03 14:10:40 -0400

    call API (/completed_steps & /VerifyMobile & /mobiles/retry) and few condition() &import /utils/helpers and react-toastif

--- src/components/VerifyMobile/VerifyMobile.js
+++ src/components/VerifyMobile/VerifyMobile.js
@@ -1,33 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import axiosInstance from '../../axiosInstance'
-
+import { currentUser } from '../utils/helpers'
 import './verifyMobile.css';
 
 
 const VerifyMobile = (props) => {
     const [ values,setValues] = useState({})
+    
+    
     const handleChange = (event) =>{
       setValues({
         ...values,
         [event.target.name]: event.target.value,
       });
     }
+
+    useEffect(() => {
+      async function onLoad() {
+        try {
+         const response = await axiosInstance.get('/profiles/completed_steps',{},{timeout: 5000})
+         //  debugger
+         if(response.data.mobile_verified){
+            if(!currentUser().name){
+              props.history.push('/updateUser')
+            }
+            props.history.push('/home')   
+         }        
+        } catch (e) {
+          alert(e);
+        }
+      }
+      onLoad()
+    },[])
    
     const handleSubmit = (event) => {
         event.preventDefault()
-        axiosInstance.put('https://wedded-raghu.herokuapp.com/api/v1/mobiles/verify',values).then((resonse) =>{
-            //debugger  
+        // debugger
+        axiosInstance.put('/mobiles/verify',values).then((resonse) =>{
+          if(!currentUser().name){
+            props.history.push('/updateUser')
+          }else{
             props.history.push('/home')
+          }
         }).catch((error) =>{
+          // debugger
+          toast.error(error?.response?.data?.errors)
         })
     }
-
+    
     const generateOtp = () => { 
-        axiosInstance.put('https://wedded-raghu.herokuapp.com/api/v1/mobiles/retry',{},{timeout: 5000}).then((resonse) =>{
+      // debugger
+        axiosInstance.put('/mobiles/retry',{},{timeout: 5000}).then((resonse) =>{
+          toast.success('OTP has sent')
           }).catch((error) =>{
-            //debugger
-        })
+            toast.error(error?.response?.data?.errors)
+          })
     }
 
     return(
