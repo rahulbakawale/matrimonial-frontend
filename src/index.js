b0d59f3b94aafbe4f46bb03e1fae4fb9aa4acda3
rahul <rahul@kliftox.com>
2021-08-07 19:37:17 -0400

    Add all css-files and images and (import All css files src/index.js)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'assets/css/all.css'
+import 'assets/css/animate.min.css'
+import 'assets/css/custom_form.css'
+import 'assets/css/form-style.css'
+import 'assets/css/mdtimepicker.css'
+import 'assets/css/modalizer.css'
+import 'assets/css/style.css'
+import 'assets/css/select2.min.css'
+import 'assets/css/style1.css'
+
 
 export const Main = () => {
 
