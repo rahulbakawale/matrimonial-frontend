8f3955eb43d9df6ef792a5bf7477af806f6f2fc7
rahul <rahul@kliftox.com>
2021-08-09 11:45:00 -0400

    code implementation login view page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -3,7 +3,6 @@ import { withRouter } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 import axiosInstance from '../../axiosInstance'
-import './login.css';
 const Login = (props) => {
     const [ values,setValues] = useState({})
     const handleChange = (event) =>{
@@ -28,21 +27,35 @@ const Login = (props) => {
         })
     }
     return(
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form onSubmit={(event) => handleSubmit(event)} >
-        <label>
-          <h2>Email</h2>
-          <input type="text" name='mobile' onChange={handleChange} />
-        </label>
-        <label>
-          <h2>Password</h2>
-          <input type="password" name='password' onChange={handleChange}/>
-        </label>
-        <div>
-        <button type="submit">Submit</button>
+    <div className="modal query_modal modalizer animate__animated animate__fast" id="login" tabindex="-1" role="dialog" data-animate-in="zoomIn" data-animate-out="zoomOut">
+        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Login</h5>
+              <button type="button" className="close align-self-end closemodal">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div className="modal-body mx-0 d-flex flex-column">
+              <div className="query_form">
+                <form onSubmit={(event) => handleSubmit(event)}>
+                  <div className="form-group">
+                  <input type="text" name='mobile' onChange={handleChange} className="form-control" required />
+                    <label for="name">Mobile</label>
+                  </div>
+                  <div className="form-group">
+                    <input type="password" name='password' onChange={handleChange} className="form-control" required />
+                    <label for="password">Password</label>
+                  </div>
+                  <button type="submit" className="btn query_btn">Login</button>
+                  <span className="login_txt">New Member to Zodiacs?
+                    <a className="nav-link modalinit" href="javascript:;" onClick={() => window.openSignUpModal() } >Sign Up</a>
+                  </span>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-      </form>
       </div>
     )
   }
