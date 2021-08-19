4fde4c89ae6a19f73a67e3b615f847f24983bbf7
rahul <rahul@kliftox.com>
2021-08-19 19:27:33 -0400

    create function call API completed_step

--- src/components/utils/helpers.js
+++ src/components/utils/helpers.js
@@ -1,7 +1,30 @@
+import axiosInstance from 'axiosInstance'
+
+
 export const isLogin = () => {
     return !!localStorage.user
 }
 
 export const currentUser = () => {
     return localStorage.user && JSON.parse(localStorage.user)
+}
+
+export const getCompleteStep = async(headersData) => {
+    let res;
+    if(headersData){
+        const headers = {
+            'access-token': headersData['access-token'],
+            'client': headersData['uid'],
+            'uid': headersData["client"],
+        }
+        res = await axiosInstance.get(`/profiles/completed_steps`,{ headers: headers })
+    }else{
+        res = await axiosInstance.get(`/profiles/completed_steps`)
+    }
+    debugger
+    localStorage.setItem('completeStep',JSON.stringify(res.data))
+}
+
+export const completeStep = () => {
+    localStorage.getItem('completeStep') && JSON.parse(localStorage.completeStep)
 }
