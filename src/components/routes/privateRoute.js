09d32616133ebc04ebd84fe72888aca7903ae3e0
rahul <rahul@kliftox.com>
2021-08-23 13:31:30 -0400

    used few condition  and  check currentProfile status

--- src/components/routes/privateRoute.js
+++ src/components/routes/privateRoute.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { completeStep, isLogin } from '../utils/helpers';
-
+import { completeStep, isLogin, currentProfile} from '../utils/helpers';
+import { AppRoutes } from 'constants/routes';
 const PrivateRoute = ({component: Component, ...rest}) => {
-    const mobileVerified = completeStep()?.mobile_verified
+
+
+
+    const renderRoutes = (props) => {
+       const pathname = props.location.pathname
+       const mobileVerified = completeStep()?.mobile_verified
+       if(isLogin()){
+          if(mobileVerified){
+            return (pathname === AppRoutes.QUALIFICATIONS && currentProfile() ) ? <Component {...props} /> : <Redirect to={AppRoutes.PROFILES} />
+          }else{
+            return<Redirect to="/verifyMobile" />
+          }
+       }else{
+           return( <Redirect to='/' />)
+       }
+    }
 
     return (
         <Route {...rest} render={props => {
-            return(
-            isLogin() ? (mobileVerified ? <Component {...props} /> : <Redirect to="/verifyMobile" />)
-            : <Redirect to="/" />
-        ) } } />
+            return renderRoutes(props)
+         } } />
     );
 };
 
