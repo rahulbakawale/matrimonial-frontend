756678fa978fa97d049670a7edab5da0013d84ef
rahul <rahul@kliftox.com>
2021-08-24 19:57:31 -0400

    template implementation and few changes

--- src/components/Sibling/sibling.js
+++ src/components/Sibling/sibling.js
@@ -38,87 +38,68 @@ const Sibling = (props) => {
       }
 return(
 <>
-<section class="form_section login_form">
-   <div class="form_header">
-      <div class="container">
-         <a class="form_logo" href="#">
-         <img src={logoImg} className="img-fluid" alt=""  />
-         </a>
-      </div>
-   </div>
-   <div class="container">
-      <div class="login-reg-panel">
-         <div class="login-info-box" style={{ display: 'none'}}>
-         <h2>Have an account?</h2>
-         <p>Lorem ipsum dolor sit amet</p>
-         <div class="logreg_form">
-            <label id="label-register" for="log-reg-show">Login</label>
-            <input type="radio" name="active-log-panel" id="log-reg-show" checked="checked" />
-         </div>
-      </div>
-      <div class="white-panel user_info_form">
-         <div class="login-show">
-            <h2>Sibling</h2>
-            <form onSubmit={(event) =>
-               handleSubmit(event)}>
-               <div className="form-group">
-                  <input type="text" name='name' onChange={handleChange} classNameName="form-control" required />
-                  <label for="mtrprofession">Name</label>
-               </div>
-               <div className="form-group">
-                  <input type="text" name='profession' onChange={handleChange} classNameName="form-control" required />
-                  <label for="mtrprofession">Profession</label>
-               </div>
-               <div className="form-group">
-                  <input type="number" name='age' onChange={handleChange} classNameName="form-control" required />
-                  <label for="mtrothdtl">Age</label>
-               </div>
-               <div class="form-group switch_btn">
-                  <h6>Married</h6>
-                  <label class="switch">
-                  <input type="checkbox" name='married' onChange={ handleRadio } required />
-                  <span class="slider round"></span>
-                  </label>
-               </div>
-               <div className="form-group">
-                  <div className="row">
-                     <div className="col-md-3 col-sm-3 col-12">
-                        <h6>Gender</h6>
-                     </div>
-                     <div className="col-md-9 col-sm-9 col-12">
-                        <div className="radio_opst">
-                           <label>
-                           <input type="radio" onChange={ handleRadioChange } name="gender" value='male'/>
-                           <span className="rddesign"></span>
-                           <span className="text">Male</span>
-                           </label>
-                           <label>
-                           <input type="radio"  onChange={ handleRadioChange } name="gender" value='female'/>
-                           <span className="rddesign"></span>
-                           <span className="text">Female</span>
-                           </label>
+<section class="form_section">
+    <div class="form_header">
+        <div class="container">
+            <img src={logoImg} className="img-fluid" alt=""  />
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
+                <div class="user_info_form">
+                    <h2 class="form_heading">Siblings Details</h2>
+                    <form onSubmit={(event) =>
+                        handleSubmit(event)}>
+                        <div className="form-group">
+                            <input type="text" name='name' onChange={handleChange} classNameName="form-control" required />
+                            <label for="mtrprofession">Name</label>
                         </div>
-                     </div>
-                  </div>
-               </div>
-               <button type="submit" className="btn log_reg_btn">Submit</button>
-            </form>
-         </div>
-      </div>
-   </div>
-   </div>
+                        <div className="form-group">
+                            <input type="text" name='profession' onChange={handleChange} classNameName="form-control" required />
+                            <label for="mtrprofession">Profession</label>
+                        </div>
+                        <div className="form-group">
+                            <input type="number" name='age' onChange={handleChange} classNameName="form-control" required />
+                            <label for="mtrothdtl">Age</label>
+                        </div>
+                        <div class="form-group switch_btn">
+                            <h6>Married</h6>
+                            <label class="switch">
+                            <input type="checkbox" name='married' onChange={ handleRadio } required />
+                            <span class="slider round"></span>
+                            </label>
+                        </div>
+                        <div className="form-group">
+                            <div className="row">
+                                <div className="col-md-3 col-sm-3 col-12">
+                                    <h6>Gender</h6>
+                                </div>
+                                <div className="col-md-9 col-sm-9 col-12">
+                                    <div className="radio_opst">
+                                        <label>
+                                        <input type="radio" onChange={ handleRadioChange } name="gender" value='male'/>
+                                        <span className="rddesign"></span>
+                                        <span className="text">Male</span>
+                                        </label>
+                                        <label>
+                                        <input type="radio"  onChange={ handleRadioChange } name="gender" value='female'/>
+                                        <span className="rddesign"></span>
+                                        <span className="text">Female</span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" className="btn log_reg_btn">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 </>
 )
 }
 
   export default withRouter(Sibling)
-
-//  import React from 'react';
-//  const  Sibling= () => {
-//      return(
-//         <h1> tesr </h1>
-//      )
-//  }
-
-//  export default(Sibling)
