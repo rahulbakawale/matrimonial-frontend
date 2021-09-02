55235474bac73d7d4734fe2d3f7a64bef1e1b061
rahul <rahul@kliftox.com>
2021-09-01 22:25:22 -0400

    create userSetting functionality and  template implementation

---
+++ src/components/userSettings/userSetting.js
@@ -0,0 +1,102 @@
+import React, { useState} from 'react';
+import { withRouter } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import logoImg from 'assets/images/logo.png'
+import axiosInstance from '../../axiosInstance'
+
+
+const UserSetting = (props) => {
+    const [ values,setValues] = useState({marked_as_favourite_notification: false, interested_profile_notification: false, view_profile_notification: false})
+    const handleChange = (event) =>{
+      setValues({
+        ...values,
+        [event.target.name]: event.target.value,
+      });
+    }
+
+    const handleRadio = (event) => {
+        setValues({
+          ...values,
+          [event.target.name]: event.target.checked
+        })
+      }
+  
+      const handleSubmit = (event) => {   
+        event.preventDefault()
+        axiosInstance.put('/settings',values).then((response) =>{ 
+            debugger  
+          props.history.push('/')
+          }).catch((error) =>{
+            toast.error(error?.response?.data?.errors[0])
+          })
+    }
+
+
+    return(
+        <>
+            <section className="form_section">
+                <div className="form_header">
+                    <div className="container">
+                        <a class="logo" href="#">  
+                        <img src={logoImg} className="img-fluid" alt=""  />
+                        </a>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
+                            <div className="user_info_form">
+                                <h2 class="form_heading">User Setting</h2>
+                                <form onSubmit={(event) =>
+                                    handleSubmit(event)}>
+                                    <div className="form-group">
+                                        <select onChange={handleChange} name='image_visibility' className="operator form-control user_relation" required >
+                                            <option value selected={true } disabled={ true } >Image Visibility</option>
+                                            {
+                                            ["all_users", "show_interest_on_your_profile"].map((item,index)=> 
+                                            <option key={ index } value={ item } >{ item }</option>
+                                            )
+                                            }
+                                        </select>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-md-9 col-sm-9 col-12">
+                                            <div className="form-group switch_btn">
+                                                <h6>View Profile Notification</h6>
+                                                <label className="switch">
+                                                <input type="checkbox" name='view_profile_notification' onChange={ handleRadio }  />
+                                                <span className="slider round"></span>
+                                                </label>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-9 col-sm-9 col-12">
+                                            <div className="form-group switch_btn">
+                                                <h6>Interested Profile Notification</h6>
+                                                <label className="switch">
+                                                <input type="checkbox" name='interested_profile_notification' onChange={ handleRadio }  />
+                                                <span className="slider round"></span>
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-9 col-sm-9 col-12">
+                                        <div className="form-group switch_btn">
+                                            <h6>Marked as favourite notification</h6>
+                                            <label className="switch">
+                                            <input type="checkbox" name='marked_as_favourite_notification' onChange={ handleRadio }  />
+                                            <span className="slider round"></span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <button type="submit" className="btn log_reg_btn">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default withRouter(UserSetting)
