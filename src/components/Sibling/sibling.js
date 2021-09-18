851ca8449d7fdb3b0bc7b563278eef0ab0e3b121
rahul <rahul@kliftox.com>
2021-09-17 23:22:50 -0400

    fixed issue designing and few changes siblings page

--- src/components/Sibling/sibling.js
+++ src/components/Sibling/sibling.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { toast } from 'react-toastify';
-// import logoImg from 'assets/images/logo.png'
+import logoImg from 'assets/images/logo.png'
 import { Formik,Field } from 'formik';
 
 import axiosInstance from '../../axiosInstance'
 
 const Sibling = (props) => {
     const id = props?.location?.pathname?.match(/\d+/) && props?.location?.pathname?.match(/\d+/)[0]
-    const [ sibling, setSibling ] = useState({})
+    const [ sibling, setSibling ] = useState({married: false})
 
     useEffect(() => { 
         async function getSibling() {
@@ -16,116 +16,129 @@ const Sibling = (props) => {
             setSibling(result.data)
         }
         id && getSibling()
-    },[])
+     },[])
 
   
       const handleSubmit = (values) => {
         if(id){
-            axiosInstance.put(`/siblings/${ id }`,values).then((response) =>{   
-                props.history.push('/user-profiles')
+            axiosInstance.put(`/siblings/${ id }`,values).then((response) =>{ 
+                props.history.push('/')
+                props.history.push(`/user-profiles/{id}`);
+  
             }).catch((error) =>{
                 toast.error(error?.response?.data?.errors[0])
             })
 
         }else{
-            axiosInstance.post('/siblings',values).then((response) =>{   
-                props.history.push('/user-profiles')
+            axiosInstance.post('/siblings',values).then((response) =>{ 
+                props.history.push('/')
+                props.history.push(`/user-profiles/{id}`);
+                // props. history.push("/user-profiles/:id", { id });
+
             }).catch((error) =>{
                 toast.error(error?.response?.data?.errors[0])
             })
-        }     
-       
-      }
-return(
-<>
-<section class="form_section">
-    <div class="form_header">
-          {/* <a className="logo" href="#">
-                  <img src={ logoImg } className="img-fluid" alt=""  />
-               </a>             */}
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
+        }      
+    }
+    return(
+        <>
+        <section class="form_section">
+        <div className="form_header">
+          <div className="container">
+            <a className="logo" href="#">
+            <img src={logoImg} className="img-fluid" alt=""  />
+            </a>
+          </div>
+        </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
                 <div class="user_info_form">
-                    <h2 class="form_heading">Siblings Details</h2>
-                    <Formik
-              enableReinitialize
-              initialValues={ sibling }
-              validate={values =>
-              {
-              }}
-              onSubmit={(values) => {
-              handleSubmit(values)
-              }}
-              >
-              {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleSubmit,
-              isSubmitting,
-              /* and other goodies */
-              }) => {
-
-                return(
+                  <h2 class="form_heading">Siblings Details</h2>
+                  <Formik
+                    enableReinitialize
+                    initialValues={ sibling }
+                    validate={values =>
+                    {
+                    }}
+                    onSubmit={(values) => {
+                    handleSubmit(values)
+                    }}
+                    >
+                    {({
+                    values,
+                    errors,
+                    touched,
+                    handleChange,
+                    handleSubmit,
+                    isSubmitting,
+                    /* and other goodies */
+                    }) => {
+                    return(
                     <form onSubmit={(event) =>
-                        handleSubmit(event)}>
-                        <div className="form-group">
-                            <input type="text" name='name' value={ values.name } onChange={handleChange} classNameName="form-control" required />
-                            <label for="mtrprofession">Name</label>
-                        </div>
-                        <div className="form-group">
-                            <input type="text" name='profession' value={ values.profession } onChange={handleChange} classNameName="form-control" required />
-                            <label for="mtrprofession">Profession</label>
-                        </div>
-                        <div className="form-group">
+                      handleSubmit(event)}>
+                      <div className="form-group">
+                        <input type="text" name='name' value={ values.name } onChange={handleChange} classNameName="form-control" required />
+                        <label for="mtrprofession">Name</label>
+                      </div>
+                      <div className="row">
+                        <div className="col-6 col-md-6 col-sm-6 col-12">
+                          <div className="form-group">
                             <input type="number" name='age' value={ values.age } onChange={handleChange} classNameName="form-control" required />
                             <label for="mtrothdtl">Age</label>
+                          </div>
                         </div>
-
-                        <div className="form-group switch_btn">
-                            <h6>Married</h6>
-                            <label className="switch">
-                                <Field type="checkbox" name="married" />
-                                <span className="slider round"></span>
-                            </label>
+                        <div className="col-6 col-md-6 col-sm-6 col-12">
+                          <div class="form-group">
+                            <select onChange={handleChange} name='profession' value={ values.profession } class="operator form-control user_relation" required >
+                              <option value selected={true } disabled={ true } >Profession</option>
+                              {
+                              ["Doctor", "Engineer", "Advocate", "Student", "Banker", "Other"].map((item,index)=> 
+                              <option key={ index } value={ item } >{ item }</option>
+                              )
+                              }
+                            </select>
+                          </div>
                         </div>
-                      
-
-                        <div className="form-group">
-                            <div className="row">
-                                <div className="col-md-3 col-sm-3 col-12">
-                                    <h6>Gender</h6>
-                                </div>
-                                <div className="col-md-9 col-sm-9 col-12">
-                                    <div className="radio_opst">
-                                        <label>
-                                            <Field type="radio" name="gender" value='male' />
-                                            <span className="rddesign"></span>
-                                            <span className="text">Male</span>
-                                        </label>
-                                        <label>
-                                            <Field type="radio" name="gender" value='female' />
-                                            <span className="rddesign"></span>
-                                            <span className="text">Female</span>
-                                        </label>
-                                    </div>
-                                </div>
+                      </div>
+                      <div className="form-group">
+                        <div className="row">
+                          <div className="col-md-3 col-sm-3 col-12">
+                            <h6>Gender</h6>
+                          </div>
+                          <div className="col-md-9 col-sm-9 col-12">
+                            <div className="radio_opst">
+                              <label>
+                                <Field type="radio" name="gender" value='male' />
+                                <span className="rddesign"></span>
+                                <span className="text">Male</span>
+                              </label>
+                              <label>
+                                <Field type="radio" name="gender" value='female' />
+                                <span className="rddesign"></span>
+                                <span className="text">Female</span>
+                              </label>
                             </div>
+                          </div>
                         </div>
-                        <button type="submit" className="btn log_reg_btn">Submit</button>
+                      </div>
+                      <div className="form-group switch_btn">
+                        <h6>Married</h6>
+                        <label className="switch">
+                          <Field type="checkbox" name="married" />
+                          <span className="slider round"></span>
+                        </label>
+                      </div>
+                      <button type="submit" className="btn log_reg_btn">{ id ? 'Update' : 'Submit'}</button>
                     </form>
                     )}}
-                </Formik>
+                  </Formik>
                 </div>
+              </div>
             </div>
-        </div>
-    </div>
-</section>
-</>
-)
+          </div>
+        </section>
+        </>
+    )
 }
-
-  export default withRouter(Sibling)
+export default withRouter(Sibling)
