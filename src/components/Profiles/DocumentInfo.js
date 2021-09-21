3559b5e454b390d293c3f64625fb7a56806ed61a
rahul <rahul@kliftox.com>
2021-09-21 12:16:02 -0400

    fixed documentInfo page and call API (document_image) and  fixed design

--- src/components/Profiles/DocumentInfo.js
+++ src/components/Profiles/DocumentInfo.js
@@ -4,14 +4,16 @@ import { toast } from 'react-toastify';
 import { getCompleteStep } from 'components/utils/helpers';
 import { completeStep } from 'components/utils/helpers';
 import axiosInstance from '../../axiosInstance';
-//  import logoImg from 'assets/images/logo.png'
- import { Formik } from 'formik';
+import logoImg from 'assets/images/logo.png'
+import { Formik } from 'formik';
 
 
 
 const DocumentInfo = (props) => {
-  const id = completeStep()?.profile?.id && props?.match?.params?.id
+  const id = completeStep()?.profile?.id || props?.match?.params?.id
   const [ document, setDocument ] = useState({})
+
+  const [ fileData , setFileData ] = useState(null)
   useEffect(() => {
     async function onLoad(){
        const response = await axiosInstance.get(`/profiles/${ id }`,{timeout: 5000}) 
@@ -31,26 +33,26 @@ const DocumentInfo = (props) => {
 //     }
   
       const handleSubmit = (values) => {   
-        axiosInstance.put(`profiles/${id}/documents`,values).then((response) =>{ 
-          getCompleteStep(response.headers)
-          localStorage.setItem('document_ids',JSON.stringify(response.data))
-        //   props.history.push('/')
-          props.history.push('/user-profiles')
+
+        const formData = new FormData();
+        formData.append('image', fileData);
+        axiosInstance.put(`profiles/${id}/documents`,values).then(async(response) =>{ 
+            debugger
+            const result = await axiosInstance.post(`documents/${ response.data.id }/document_image`,formData)
+            console.log(result)
+            debugger
           }).catch((error) =>{
-            toast.error(error?.response?.data?.errors[0])
+            toast.error(error?.response?.data?.errors)
           })
       }
-  
-
-      console.log(DocumentInfo)   
       return(
       <>
       <section className="form_section">
           <div className="form_header">
               <div className="container">
-                  {/* <a className="logo" href="#">
+                  <a className="logo" href="#">
                   <img src={ logoImg } className="img-fluid" alt=""  />
-                  </a>             */}
+                  </a>            
               </div>
           </div>
           <Formik
@@ -82,10 +84,9 @@ const DocumentInfo = (props) => {
                               <h2 class="form_heading">Document  Info</h2>
                               <form onSubmit={(event) =>
                                   handleSubmit(event)}>
-                                  <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
                                       <div class="form-group">
                                           <select onChange={handleChange} name='id_type' value={ values.id_type } class="operator form-control user_relation" required >
-                                              <option value selected={true } disabled={ true } >Your Cast</option>
+                                              <option value selected={true } disabled={ true } >Select Id</option>
                                               {
                                               ['AadharCard', 'DrivingLicence', 'Passport', 'VoterId', 'PanCard'].map((item,index)=> 
                                               <option key={ index } value={ item } >{ item }</option>
@@ -93,14 +94,22 @@ const DocumentInfo = (props) => {
                                               }
                                           </select>
                                       </div>
-                                  </div>
                                   { (values.id_type === 'AadharCard' || values.id_type === 'DrivingLicence' || values.id_type === 'VoterId' || values.id_type === 'Passport' || values.id_type === 'PanCard') && 
-                                  <div class="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
                                       <div class="form-group">
                                           <input type="text" name='id_number' value={ values.id_number } onChange={handleChange} classNameName="form-control" required />
                                           <label for="mtrprofession">Id Number</label>
                                       </div>
-                                  </div>
+                                  }<br/>
+
+                                  { (values.id_type === 'AadharCard' || values.id_type === 'DrivingLicence' || values.id_type === 'VoterId' || values.id_type === 'Passport' || values.id_type === 'PanCard') && 
+
+                                        <div class="form-group img_upld_file">
+                                            <input type="file" id="file" 
+                                            onChange={ (event) => setFileData(event.target.files[ 0 ]) } />
+                                            <label for="file" class="btn-3">
+                                                <span>Upload Document Image</span>
+                                            </label>
+                                        </div>
                                   }
                                   <button type="submit" className="btn log_reg_btn">{ id ? 'Update' : 'Submit'}</button>
                               </form>
