4e25faa368aba03a44d6e6ad9f66fc0c35ca217e
rahul <rahul@kliftox.com>
2021-10-08 19:03:09 -0400

    create Logout button in header

--- src/components/shared/header.js
+++ src/components/shared/header.js
@@ -5,34 +5,41 @@ import Query from '../query/query';
 import NotificationDropDown from './notification-dropdown';
 
 const Header = (props) => {
+  const handleClick = () =>{
+    localStorage.clear();
+    props.history.push('/')
+    }
   const path = props.location.pathname
     return(
     <>
-      <nav class={`navbar navbar_menu navbar-expand-lg fixed-top ${ (path !== '/notification' && path !== '/favorite-profile') && 'nav_white' }`} id="sticky">
-        <div class="container">
-          <a class="logo" href="#">
-            <img src={ logoImg } class="img-fluid" />
+      <nav className={`navbar navbar_menu navbar-expand-lg fixed-top ${ (path !== '/notification' && path !== '/favorite-profile') && 'nav_white' }`} id="sticky">
+        <div className="container">
+          <a className="logo" href="#">
+            <img src={ logoImg } className="img-fluid" />
           </a>
-          <button class="navbar-toggler menu-toggle" type="button" data-toggle="collapse" data-target="#mobile_nav" aria-controls="mobile_nav" aria-expanded="false" aria-label="Toggle navigation">
-            <div class="mbmenu-icon"><span></span><span></span><span></span></div>
+          <button className="navbar-toggler menu-toggle" type="button" data-toggle="collapse" data-target="#mobile_nav" aria-controls="mobile_nav" aria-expanded="false" aria-label="Toggle navigation">
+            <div className="mbmenu-icon"><span></span><span></span><span></span></div>
           </button>
-          <div class="collapse navbar-collapse" id="mobile_nav">
-            <ul class="navbar-nav menubar ml-auto">
-              <li class="menu_link active">
+          <div className="collapse navbar-collapse" id="mobile_nav">
+            <ul className="navbar-nav menubar ml-auto">
+              <li className="menu_link active">
                 <Link to={ `/`}  className="breadcrumb-item"><i className="breadcrumb-item" ><a href="#">Home</a></i></Link>
               </li>
-              <li class="menu_link">
+              <li className="menu_link">
                 <a className="nav-link modalinit" href="javascript:;" data-toggle="modal" data-modal="query">About Us</a>
-                {/* <a class="nav-link hvr-grow" href="javascript:;">About Us</a> */}
+                {/* <a className="nav-link hvr-grow" href="javascript:;">About Us</a> */}
               </li>
-              <li class="menu_link">
-                <a class="nav-link hvr-grow" href="javascript:;">FAQ</a>
+              <li className="menu_link">
+                <a className="nav-link hvr-grow" href="javascript:;">FAQ</a>
               </li>
-              <li class="menu_link">
-                <a class="nav-link hvr-grow" href="javascript:;">Contact Us</a>
+              <li className="menu_link">
+                <a className="nav-link hvr-grow" href="javascript:;">Contact Us</a>
               </li>
-              <li class="dropdown ntf_btn">
+              <li className="dropdown ntf_btn">
                 <NotificationDropDown />       
+              </li>
+              <li className="menu_link">
+                <button type="submit" className="btn query_btn_denger" onClick={handleClick}>Logout</button>
               </li>
             </ul>
             <Query/>
