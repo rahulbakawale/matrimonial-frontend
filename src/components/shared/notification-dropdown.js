9513dc750b4356040a8e72f4d4df9be120378950
rahul <rahul@kliftox.com>
2021-10-14 08:04:31 -0400

    create User-setting LINK

--- src/components/shared/notification-dropdown.js
+++ src/components/shared/notification-dropdown.js
@@ -19,8 +19,15 @@ const NotificationDropDown = () => {
     }
    onLoad()
   },[])
-  return( 
-    <>
+  return(          
+  <>
+    <li class="dropdown ntf_btn">
+      <div class="dropdown-menu ntf_box" aria-labelledby="ntf_bell">
+        <div class="ntf_heading">
+          <h5>Notification</h5>
+          <Link to={`/user-setting`} href="javascript:;"><i className="fas fa-cog"></i></Link>
+        </div>
+      </div>
       <a class="nav-link ntf_bell" href="#" id="ntf_bell" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
         <i class="fas fa-bell"></i>
         <mark class="rubberBand">{ count }</mark>
@@ -36,7 +43,8 @@ const NotificationDropDown = () => {
           <li><Link to='/notification'>See more</Link></li> 
         </ul>
       </div>
-    </>
+    </li>
+  </>
   )
 }
 
