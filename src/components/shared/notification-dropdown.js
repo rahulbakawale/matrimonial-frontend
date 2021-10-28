fa4dafae7a907bdf644883c80aaeace591ca9f3a
rahul <rahul@kliftox.com>
2021-10-28 16:26:22 -0400

    change form User-setting-page and use Formik and set value

--- src/components/shared/notification-dropdown.js
+++ src/components/shared/notification-dropdown.js
@@ -22,24 +22,30 @@ const NotificationDropDown = () => {
   return(          
   <>
     <li class="dropdown ntf_btn">
-      <div class="dropdown-menu ntf_box" aria-labelledby="ntf_bell">
+      {/* <div class="dropdown-menu ntf_box" aria-labelledby="ntf_bell">
         <div class="ntf_heading">
           <h5>Notification</h5>
           <Link to={`/user-setting`} href="javascript:;"><i className="fas fa-cog"></i></Link>
         </div>
-      </div>
+      </div> */}
       <a class="nav-link ntf_bell" href="#" id="ntf_bell" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
         <i class="fas fa-bell"></i>
+        
         <mark class="rubberBand">{ count }</mark>
       </a>
+      
       <div class="dropdown-menu ntf_box" aria-labelledby="ntf_bell">
         <ul class="list-unstyled notification-list-style">
+          <div class="ntf_heading">
+            <h5>Notification</h5>
+            <Link to={`/user-setting`} href="javascript:;"><i className="fas fa-cog"></i></Link>
+          </div>
           { notifications.map((item,index) => {
             return( <li>
               <img src={ userImg } width='35px' alt={`user-s-${ index }` } />
               <span>{item?.title}</span>
             </li>)
-          })}
+          })}       
           <li><Link to='/notification'>See more</Link></li> 
         </ul>
       </div>
--- src/components/userSettings/userSetting.js
+++ src/components/userSettings/userSetting.js
@@ -1,27 +1,26 @@
-import React, { useState} from 'react';
+import React from 'react';
 import { withRouter } from 'react-router-dom';
 import logoImg from 'assets/images/logo.png';
 import { toast } from 'react-toastify';
 import { getCompleteStep } from 'components/utils/helpers';
-
-import axiosInstance from '../../axiosInstance'
+import axiosInstance from '../../axiosInstance';
+import { Formik,Field } from 'formik';
 
 const UserSetting = (props) => {
-  const [ values,setValues] = useState({marked_as_favourite_notification: false, interested_profile_notification: false, view_profile_notification: false})
-    const handleChange = (event) =>{
-      setValues({
-        ...values,
-        [event.target.name]: event.target.value,
-      });
-    }
-    const handleRadio = (event) => {
-        setValues({
-        ...values,
-        [event.target.name]: event.target.checked
-      });
-    }
-    const handleSubmit = (event) => {   
-      event.preventDefault()
+  // const [ values,setValues] = useState({marked_as_favourite_notification: false, interested_profile_notification: false, view_profile_notification: false})
+    // const handleChange = (event) =>{
+    //   setValues({
+    //     ...values,
+    //     [event.target.name]: event.target.value,
+    //   });
+    // }
+    // const handleRadio = (event) => {
+    //     setValues({
+    //     ...values,
+    //     [event.target.name]: event.target.checked
+    //   });
+    // }
+    const handleSubmit = (values) => {   
       axiosInstance.put('/settings',values).then(async(response) =>{ 
         await getCompleteStep(response.headers)
         props.history.push('/user-profiles')
@@ -40,64 +39,88 @@ const UserSetting = (props) => {
             </a>
           </div>
         </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
-              <div className="user_info_form">
-                <h2 class="form_heading">User Setting</h2>
-                <form onSubmit={(event) =>handleSubmit(event)}>
-                  <div className="form-group">
-                    <select onChange={handleChange} name='image_visibility' className="operator form-control user_relation" required >
-                      <option value selected={true } disabled={ true } >Image Visibility</option>
-                      {
-                      ["all_users", "show_interest_on_your_profile"].map((item,index)=> 
-                      <option key={ index } value={ item } >{ item }</option>
-                      )
-                      }
-                    </select>
+        <Formik
+            enableReinitialize
+            initialValues={UserSetting ,{marked_as_favourite_notification: false, interested_profile_notification: false, view_profile_notification: false}}
+            validate={values =>
+            {
+            }}
+            onSubmit={(values) => {
+            handleSubmit(values)
+            }}
+            >
+            {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+            }) => {
+            return(
+              <div className="container">
+              <div className="row">
+                <div className="col-lg-8 offset-lg-2 col-md-10 offset-md-1 col-sm-12 col-12">
+                  <div className="user_info_form">
+                    <h2 class="form_heading">User Setting</h2>
+                    <form onSubmit={(event) =>handleSubmit(event)}>
+                      <div className="form-group">
+                        <select onChange={handleChange} name='image_visibility' className="operator form-control user_relation" required >
+                          <option value selected={true } disabled={ true } >Image Visibility</option>
+                          {
+                          ["all_users", "show_interest_on_your_profile"].map((item,index)=> 
+                          <option key={ index } value={ item } >{ item }</option>
+                          )
+                          }
+                        </select>
+                      </div>
+                      { (values.image_visibility === 'all_users' || values.image_visibility === 'show_interest_on_your_profile') &&
+                        <div className="row">
+                          <div className="col-md-12 col-sm-12 col-12">
+                            <div className="form-group switch_btn">
+                              <h6>Interested Profile Notification</h6>
+                              <label className="switch">
+                                <Field type="checkbox" name='interested_profile_notification' />
+                                <span className="slider round"></span>
+                                <span class="absolute-no">NO</span>
+                              </label>
+                            </div>
+                          </div>
+                        { (values.image_visibility === 'all_users')  && <>
+                          <div className="col-md-12 col-sm-12 col-12">
+                            <div className="form-group switch_btn">
+                              <h6>Marked as favourite notification</h6>
+                              <label className="switch">
+                                <Field type="checkbox" name='marked_as_favourite_notification' />
+                                <span className="slider round"></span>
+                                <span class="absolute-no">NO</span>
+                              </label>
+                            </div>
+                          </div>
+                          <div className="col-md-12 col-sm-12 col-12">
+                            <div className="form-group switch_btn">
+                              <h6>View Profile Notification</h6>
+                              <label className="switch">
+                                <Field type="checkbox" name='view_profile_notification' />
+                                <span className="slider round"></span>
+                                <span class="absolute-no">NO</span>
+                              </label>
+                            </div>
+                          </div>
+                        </>}
+                      </div>
+                      }     
+                      <button type="submit" className="btn log_reg_btn">Submit</button>
+                    </form>
                   </div>
-                  { (values.image_visibility === 'all_users' || values.image_visibility === 'show_interest_on_your_profile') &&
-                    <div className="row">
-                      <div className="col-md-12 col-sm-12 col-12">
-                        <div className="form-group switch_btn">
-                          <h6>Interested Profile Notification</h6>
-                          <label className="switch">
-                            <input type="checkbox" name='interested_profile_notification' onChange={ handleRadio }  />
-                            <span className="slider round"></span>
-                            <span class="absolute-no">NO</span>
-                          </label>
-                        </div>
-                      </div>
-                    { (values.image_visibility === 'all_users')  && <>
-                      <div className="col-md-12 col-sm-12 col-12">
-                        <div className="form-group switch_btn">
-                          <h6>Marked as favourite notification</h6>
-                          <label className="switch">
-                            <input type="checkbox" name='marked_as_favourite_notification' onChange={ handleRadio }  />
-                            <span className="slider round"></span>
-                            <span class="absolute-no">NO</span>
-                          </label>
-                        </div>
-                      </div>
-                      <div className="col-md-12 col-sm-12 col-12">
-                        <div className="form-group switch_btn">
-                          <h6>View Profile Notification</h6>
-                          <label className="switch">
-                            <input type="checkbox" name='view_profile_notification' onChange={ handleRadio }  />
-                            <span className="slider round"></span>
-                            <span class="absolute-no">NO</span>
-                          </label>
-                        </div>
-                      </div>
-                    </>}
-                  </div>
-                  }     
-                  <button type="submit" className="btn log_reg_btn">Submit</button>
-                </form>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+            
+            )}}
+        </Formik>
+
       </section>
     </>
   )
