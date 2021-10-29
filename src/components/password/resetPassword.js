524c002164120a007d9d91536615fc58ef49a096
rahul <rahul@kliftox.com>
2021-10-29 19:39:10 -0400

    name  change

--- src/components/password/resetPassword.js
+++ src/components/password/resetPassword.js
@@ -49,7 +49,7 @@ const ResetPassword = (props) => {
     axiosInstance.put('/mobiles/forget_verify',formData).then(async(response) =>{
       setOtpScreen(false)
     }).catch((error) => {
-    toast.error(error?.response?.data?.errors && error?.response?.data?.errors[0] )
+    toast.error(error?.response?.data?.errors[0] )
     })
   }
   const handleUpdatePassword = (event) => {
@@ -64,7 +64,7 @@ const ResetPassword = (props) => {
     <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
         {otpScreen ? 
         <div className="modal-content reset-password">
-          <div className="modal-header">
+          <div className="modal-header otp">
             <h5 className="modal-title">Enter OTP </h5>
             <button type="button" className="close align-self-end closemodal">
               <span aria-hidden="true">&times;</span>
@@ -74,9 +74,9 @@ const ResetPassword = (props) => {
             <div className="reset_password a">
               <form onSubmit={(event) => handleOtpSubmit(event)}>
               <input type='hidden' name='mobile' id='mobile1'/>
-                <div className="form-group">
+                <div className="form-group ">
                 <input type="text" name='otp' onChange={handleChange} className="form-control" required />
-                  <label for="name">Mobile</label>
+                  <label for="name">Enter OTP</label>
                 </div>
                 <a className="resend-a" id='link-resend'onClick={ resendOtp } >Resend OTP</a>
                 <button type="submit" className="btn query_btn">Submit</button> 
@@ -96,7 +96,6 @@ const ResetPassword = (props) => {
               <form onSubmit={(event) => handleUpdatePassword(event)}>
                 <div className="form-group">
                 <input type='hidden' name='mobile' id='mobile2'/>
-
                 <input type="text" name='password' onChange={handleChange} className="form-control" required />
                   <label for="name">Password</label>
                 </div>
